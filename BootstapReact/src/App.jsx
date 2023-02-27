7147e64f52925b68cee9033012bc36c1f62e4d9c
Sundeep <mistrysundeep@gmail.com>
2023-02-27 11:59:18 +0000

    Initial commit for Tenzies game

---
+++ BootstapReact/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+
+function App() {
+  return (
+    <main>
+      <div className="tenzies-container">
+        <div className="tenzies-main">
+
+        </div>
+      </div>
+    </main>
+  )
+}
+
+export default App
